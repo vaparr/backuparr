bdc956e89e6d292357788956a322387eef63f950
doctarrwho <doctarrwho@outlook.com>
2020-09-26 00:51:13 -0400

    add no onedrive option, logging improvements

--- backuparr.sh
+++ backuparr.sh
@@ -6,15 +6,16 @@ ONEDRIVE_LOCATION=onedrive:unraid/backup
 DEFAULT_TIMEOUT=30
 DRYRUN=""
 PROGRESS="--info=progress2"
-EXCLUDE=(profile/lock fail2ban/filter.d www/Dashboard Plex?Media?Server/Cache Plex?Media?Server/Media Plex?Media?Server/Metadata data/metadata www/nextcloud home/.icons profile/cache2 cache2/entries log/ Log/ logs/ Logs/ '*.log' log.txt '*.log.*' cache/ Caches/ '*.pid' '*.sample' '*.lock')
+EXCLUDE=(profile/lock fail2ban/filter.d www/Dashboard www/nextcloud home/.icons profile/cache2 cache2/entries log/ Log/ logs/ Logs/ '*.log' log.txt '*.log.*' Caches/ '*.pid' '*.sample' '*.lock')
 EXCLUDEPRE=('*.db' '*.xml' '*.dat' '*.dat.old' '*.db-*' '*.ini' '*.conf' '*.json' '*.ejs' BT_backup/ databases/ '*.sqlite*' '*.sqlite')
 
 now=$(date +"%Y-%m-%d")
 create_only=0
 dry_run=0
 verbose=0
+no_onedrive=0
 
-while getopts "h?cfdv" opt; do
+while getopts "h?cfdvn" opt; do
     case "$opt" in
     h | \?)
         echo Showing Help
@@ -31,22 +32,13 @@ while getopts "h?cfdv" opt; do
         verbose=1
         PROGRESS="--progress"
         ;;
+    n)
+        no_onedrive=1
+        ;;
     #f) 
         #output_file=$OPTARG
         #;;
     esac
-done
-
-containers=$(sudo docker ps -a | awk '{if(NR>1) print $NF}')
-
-exclude_opts=()
-for item in "${EXCLUDE[@]}"; do
-    exclude_opts+=(--exclude "$item")
-done
-
-exclude_opts_pre=${exclude_opts[@]}
-for item in "${EXCLUDEPRE[@]}"; do
-    exclude_opts_pre+=(--exclude "$item")
 done
 
 backup_docker() {
@@ -60,6 +52,10 @@ backup_docker() {
 
     [ "$1" == "" ] && echo Docker is a required param && return
 
+    echo =================================================================
+    echo Docker: $D_NAME [Start Time: $(date)]
+    echo =================================================================
+
     [ ! -d $T_PATH ] && mkdir -p $T_PATH
     [[ "$create_only" == "1" || "$dry_run" == "1" ]] && docker inspect $D_NAME >$T_PATH/$D_NAME-dockerconfig.json
 
@@ -68,12 +64,8 @@ backup_docker() {
         S_PATH=$(docker inspect -f '{{json .Mounts }}' $D_NAME | jq .[].Source | grep appdata/ | head -1 | cut -f 2 -d \" | tr -d '\n')
     fi
 
-    if [ "$create_only" == 1 ]; then
-        echo ------------ $D_NAME ----------------      
-    fi
-
-    [ ! -d $S_PATH ] && echo Could not find $S_PATH && return
-    [ "$S_PATH" == "" ] && echo Could not find a source path for $D_NAME && return
+    [ ! -d $S_PATH ] && echo "Could not find $S_PATH" && echo && return
+    [ "$S_PATH" == "" ] && echo "Could not find a source path for $D_NAME" && echo && return
 
     [ ! -d $D_PATH ] && mkdir -p $D_PATH
 
@@ -81,7 +73,7 @@ backup_docker() {
         touch $T_PATH/backup.config
     fi
 
-    local BACKUPCONFIG=$(cat $T_PATH/backup.config 2>/dev/null | egrep -v ^# | egrep -v ^$)
+    local BACKUPCONFIG=$(cat $T_PATH/backup.config 2>/dev/null | egrep -v ^\# | egrep -v ^$)
     if [ "$BACKUPCONFIG" == "" ]; then
         echo \# docker timeout before force kill. Set to 0 to not stop the docker when backing it up >$T_PATH/backup.config
         echo \#TIMEOUT=30 >>$T_PATH/backup.config
@@ -93,9 +85,10 @@ backup_docker() {
         echo \#FORCESTART=\"true\" >>$T_PATH/backup.config
         echo "" >>$T_PATH/backup.config
         echo \#Per Docker Excludes >>$T_PATH/backup.config
-        echo \#EXCLUDES=\(fail2ban/filter.d Plex?Media?Server/Cache \'*.tmp\'\) >>$T_PATH/backup.config
+        echo \#EXCLUDES=\(Plex?Media?Server/Cache Plex?Media?Server/Media Plex?Media?Server/Metadata data/metadata cache/ \'*.tmp\'\) >>$T_PATH/backup.config
     else
-        echo Loading Variables from $T_PATH/backup.config
+        echo PHASE 0: Load Variables from $T_PATH/backup.config
+        echo ""
         . $T_PATH/backup.config
     fi
 
@@ -105,54 +98,49 @@ backup_docker() {
     fi
 
     local pre_excludes=${exclude_opts_pre[@]}
-    local full_excludes=${exclude_opts[@]}
-    
+    local full_excludes=${exclude_opts[@]}    
 
     if [ ! "$EXCLUDES" == "" ]; then
-
         for item in "${EXCLUDES[@]}"; do
             pre_excludes+=(--exclude "$item")            
-        done
+        done        
         for item in "${EXCLUDES[@]}"; do
             full_excludes+=(--exclude "$item")
         done
-
     fi
 
-    [ ! "$BACKUP" == "true" ] && echo Skipping Docker $D_NAME && return
+    [ ! "$BACKUP" == "true" ] && echo PHASE 0: Skipping Docker $D_NAME && return
 
     local A_PATH=$T_PATH/Archive
     local A_FILE=$A_PATH/$D_NAME-${now}.tgz
     local RUNNING=$(docker container inspect -f '{{.State.Running}}' $D_NAME)
     
-    echo =================================================================
-    echo Docker: $D_NAME [Start Time: $(date)]
-    echo =================================================================
     printf "Dest Path: \t $D_PATH\n"
     printf "Source Path: \t $S_PATH\n"
     printf "Archive Path: \t $A_PATH\n"
     printf "Archive File: \t $A_FILE\n"
     printf "Running: \t $RUNNING\n"
     printf "Stop Timeout: \t $TIMEOUT\n"
-    # [ ! "$EXCLUDES" == "" ] && printf "EXCLUDES: \t $EXCLUDES\n"
+    [ ! "$EXCLUDES" == "" ] && printf "Excludes: \t (${EXCLUDES[*]})\n"
     echo ""
 
     [ ! -d $A_PATH ] && [ ! "$NUM_DAILY" == "0" ] && mkdir -p $A_PATH
     
-    echo PHASE 1: Backup existing files in $D_PATH to $A_FILE
+    echo PHASE 1: Archive $D_PATH to $A_FILE
     if [ -d $A_PATH ] && [ ! -f $A_FILE ] && [ -d $D_PATH ] && [ ! "$NUM_DAILY" == "0" ] && [ "$dry_run" == "0" ]; then        
-        echo tar -czf $A_FILE -C $D_PATH .
+        [ "$verbose" == "1" ] && echo PHASE 1: tar -czf $A_FILE -C $D_PATH .
         tar -czf $A_FILE -C $D_PATH .
     else
-        echo PHASE 1: Skipped. NUM_DAILY [$NUM_DAILY], DRY_RUN [$DRY_RUN]
+        [ -f $A_FILE ] && echo PHASE 1: Skipped. Archive exists for $now.
+        [ ! -f $A_FILE ] && echo PHASE 1: Skipped. NUM_DAILY [$NUM_DAILY], DRY_RUN [$dry_run]
     fi
     
     echo PHASE 2: Run rsync to copy files BEFORE docker stop
     if [ "$RUNNING" == "true" ] && [ ! "$TIMEOUT" == "0" ]; then        
         [ "$verbose" == "1" ] && echo PHASE 2: rsync -a $PROGRESS -h ${pre_excludes[@]} $DRYRUN $S_PATH/ $D_PATH/
         rsync -a $PROGRESS -h ${pre_excludes[@]} $DRYRUN $S_PATH/ $D_PATH/
-        echo PHASE 2: Stopping $D_NAME with timeout: $TIMEOUT
-        [ "$dry_run" == "0" ] && echo PHASE 2: Stopped docker $(docker stop -t $TIMEOUT $D_NAME)
+        echo PHASE 2: STOP $D_NAME with timeout: $TIMEOUT
+        [ "$dry_run" == "0" ] && echo PHASE 2: STOPPED docker $(docker stop -t $TIMEOUT $D_NAME)
     else
         echo PHASE 2: Skipped because either docker state [$RUNNING] is not running or Timeout [$TIMEOUT] specified as 0 to prevent docker stop.
     fi
@@ -163,10 +151,10 @@ backup_docker() {
     
     echo "PHASE 4: Start docker if previously running"
     if [[ ! "$FORCESTART" == "false" ]] || [[ "$RUNNING" == "true" && ! "$TIMEOUT" == "0" ]]; then
-        echo PHASE 4: Starting $D_NAME
-        [ "$dry_run" == "0" ] && echo PHASE 4: Started docker $(docker start $D_NAME)
+        echo PHASE 4: START $D_NAME
+        [ "$dry_run" == "0" ] && echo PHASE 4: STARTED docker $(docker start $D_NAME)
     else
-        echo PHASE 4: Skipped.. FORCESTART [$FORCESTART], RUNNING [$RUNNING], TIMEOUT [$TIMEOUT]
+        echo PHASE 4: Skipped. FORCESTART [$FORCESTART], RUNNING [$RUNNING], TIMEOUT [$TIMEOUT]
     fi
 
     [ "$dry_run" == "0" ] && [ -d $DAILY_LOCATION ] && [ ! "$NUM_DAILY" == "0" ] && find $A_PATH -mtime +${NUM_DAILY} -name '*.tgz' -delete
@@ -178,9 +166,22 @@ backup_docker() {
 }
 
 echo ""
-echo ---- Backup Started [$(date)] ----
+echo "---- Backup Started [$(date)] ----"
 echo ""
 
+# exclude options, phase 2
+exclude_opts=()
+for item in "${EXCLUDE[@]}"; do
+    exclude_opts+=(--exclude "$item")
+done
+
+# exclude options, phase 1
+exclude_opts_pre=${exclude_opts[@]}
+for item in "${EXCLUDEPRE[@]}"; do
+    exclude_opts_pre+=(--exclude "$item")
+done
+
+# flash drive backup
 if [ ! "$create_only" == "1" ]; then
     if [ -d /boot ]; then
         [ ! -d $BACKUP_LOCATION/Flash ] && mkdir -p $BACKUP_LOCATION/Flash
@@ -190,20 +191,34 @@ if [ ! "$create_only" == "1" ]; then
     fi
 fi
 
+# test
+# backup_docker "organizrv2"
+# exit
+
+# docker backup
+containers=$(sudo docker ps -a | awk '{if(NR>1) print $NF}')
 for container in $containers; do
     backup_docker $container
 done
 
-if [[] "$create_only" == "1" || "$dry_run" == "1" ]]; then
+echo "---- Backup Complete [$(date)] ----"
+echo ""
+
+if [[ "$create_only" == "1" || "$dry_run" == "1" || "$no_onedrive" == "1" ]]; then
     exit
 fi
 
-echo ---- Backup Complete [$(date)] ----
+echo "---- Starting Onedrive upload [$(date)] ----"
+echo ""
+
+if [ "$verbose" == "1" ]; then
+    echo rclone sync -v --checkers 16 --transfers 16 --fast-list --copy-links $BACKUP_LOCATION $ONEDRIVE_LOCATION
+    /usr/sbin/rclone sync -v --checkers 16 --transfers 16 --fast-list --copy-links $BACKUP_LOCATION $ONEDRIVE_LOCATION
+else
+    /usr/sbin/rclone sync --progress --checkers 16 --transfers 16 --fast-list --copy-links $BACKUP_LOCATION $ONEDRIVE_LOCATION
+fi
 
 echo ""
-echo "---- Starting Onedrive upload [$(date)] ----"
-[ "$verbose" == "1" ] && echo rclone sync -v --checkers 16 --transfers 16 --fast-list --copy-links $BACKUP_LOCATION $ONEDRIVE_LOCATION
-/usr/sbin/rclone sync -v --checkers 16 --transfers 16 --fast-list --copy-links $BACKUP_LOCATION $ONEDRIVE_LOCATION
 echo "---- Onedrive upload Complete [$(date)] ----"
 echo ""
 
