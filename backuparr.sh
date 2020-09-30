3e072a8ff84e1fc34714a880226f0a4a29a0fa7d
doctarrwho <doctarrwho@outlook.com>
2020-09-30 00:58:13 -0400

    logging changes

--- backuparr.sh
+++ backuparr.sh
@@ -19,16 +19,15 @@ skip_onedrive=0
 docker_name=""
 STOPPED_DOCKER=""
 
-
 while getopts "h?cfdvsn:" opt; do
     case "$opt" in
     h | \?)
         echo Options:
-	echo "-d : Dry Run"
-	echo "-v : Verbose"
-	echo "-s : Skip OneDrive Upload"
-	echo "-c : Create Backup.config files only"
-	echo "-n [docker] : Only backup this single docker"
+        echo "-d : Dry Run"
+        echo "-v : Verbose"
+        echo "-s : Skip OneDrive Upload"
+        echo "-c : Create Backup.config files only"
+        echo "-n [docker] : Only backup this single docker"
         exit 0
         ;;
     c)
@@ -48,37 +47,43 @@ while getopts "h?cfdvsn:" opt; do
     n)
         docker_name=${OPTARG}
         ;;
-    #f) 
+        #f)
         #output_file=$OPTARG
         #;;
     esac
 done
 
+function converttime() {
+    ((m = (${1} % 3600) / 60))
+    ((s = ${1} % 60))
+    printf "%02d minutes, %02d seconds" $m $s
+}
+
 trap 'ExitFunc' exit
 SUCCESS="false"
-function ExitFunc(){
+function ExitFunc() {
+    local time_m=$(converttime $SECONDS)
     if [[ ! "$SUCCESS" == "true" ]]; then
-       NotifyError "[Backuparr exited]" "Script exited abnormally after $SECONDS seconds."
+        NotifyError "[Backuparr exited]" "Script exited abnormally after $time_m."
     else
-       NotifyInfo "[Backuparr exited]" "Script exited normally after $SECONDS seconds."
+        NotifyInfo "[Backuparr exited]" "Script exited normally after $time_m."
     fi
 
     if [[ ! "$STOPPED_DOCKER" == "" ]]; then
-       docker start $STOPPED_DOCKER
+        docker start $STOPPED_DOCKER
     fi
-
 }
 
 function NotifyInfo() {
     if [[ $script_path =~ \/boot\/repos.* ]]; then # one-line stats when running from user scripts
-         /usr/local/emhttp/webGui/scripts/notify -e "$1" -d "$2" -i "normal" 
+        /usr/local/emhttp/webGui/scripts/notify -e "$1" -d "$2" -i "normal"
     fi
     echo $1 - $2
 }
 
 function NotifyError() {
     if [[ $script_path =~ \/boot\/repos.* ]]; then # one-line stats when running from user scripts
-         /usr/local/emhttp/webGui/scripts/notify -e "$1" -d "$2" -i "alert" 
+        /usr/local/emhttp/webGui/scripts/notify -e "$1" -d "$2" -i "alert"
     fi
     echo [ERROR] $1 - $2
 }
@@ -96,120 +101,115 @@ function LogWarning() {
 }
 
 function LogError() {
-   echo "[ERROR] $@"
-   NotifyError "Backuparr Error" "$@"
+    echo "[ERROR] $@"
+    NotifyError "Backuparr Error" "$@"
 }
 
-function stop_docker(){
+function stop_docker() {
+    local op="[DOCKER STOP]"
     local stop_seconds=$SECONDS
-    LogInfo PHASE 2: STOPPING $1 with timeout: $2
+    LogInfo $op: STOPPING $1 with timeout: $2
     local RUNNING=$(docker container inspect -f '{{.State.Running}}' $1)
 
-    if [[ "$RUNNING" == "false" ]];
-    then
-        LogInfo PHASE 2: Docker is already stopped!
+    if [[ "$RUNNING" == "false" ]]; then
+        LogInfo $op: Docker is already stopped!
         return
     fi
 
     if [ "$dry_run" == "0" ]; then
         STOPPED_DOCKER=$1
-        LogInfo PHASE 2: STOPPED docker $(docker stop -t $2 $1) in $(( SECONDS-$stop_seconds )) Seconds
+        LogInfo $op: STOPPED docker $(docker stop -t $2 $1) in $((SECONDS - $stop_seconds)) Seconds
     fi
     RUNNING=$(docker container inspect -f '{{.State.Running}}' $1)
 
-    if [[ "$RUNNING" == "false" ]];
-    then
-        LogVerbose PHASE 2: Docker Stopped Successfully
+    if [[ "$RUNNING" == "false" ]]; then
+        LogVerbose $op: Docker Stopped Successfully
     else
-        LogWarning PHASE 2: Docker not stopped.
+        LogWarning $op: Docker not stopped.
         docker stop -t 600 $1
     fi
 }
 
-function start_docker(){
+function start_docker() {
+    local op="[DOCKER START]"
     local start_seconds=$SECONDS
-    LogInfo PHASE 4: STARTING $1
+    LogInfo $op: STARTING $1
     local RUNNING=$(docker container inspect -f '{{.State.Running}}' $1)
-    if [[ "$RUNNING" == "true" ]];
-    then
-        LogInfo PHASE 4: Docker is already started!
+    if [[ "$RUNNING" == "true" ]]; then
+        LogInfo $op: Docker is already started!
         return
     fi
-    if [ "$dry_run" == "0" ];then
-       LogInfo PHASE 4: STARTED docker $(docker start $1) in $(( SECONDS-$start_seconds )) Seconds
-       STOPPED_DOCKER=""
+    if [ "$dry_run" == "0" ]; then
+        LogInfo $op: STARTED docker $(docker start $1) in $((SECONDS - $start_seconds)) Seconds
+        STOPPED_DOCKER=""
     fi
     RUNNING=$(docker container inspect -f '{{.State.Running}}' $1)
-    if [[ "$RUNNING" == "true" ]];
-    then
-        LogVerbose PHASE 4: Docker Started Successfully
+    if [[ "$RUNNING" == "true" ]]; then
+        LogVerbose $op: Docker Started Successfully
     else
-        LogWarning PHASE 4: Docker not started.
+        LogWarning $op: Docker not started.
         docker start $1
     fi
 
 }
 
-function create_config(){
-
-    [[ "$dry_run" == "1" ]] && LogInfo "Skipping config create in DryRun" && return
+function create_config() {
+    local op="[CONFIG]"
+    [[ "$dry_run" == "1" ]] && LogInfo "$op: Skipping config create in DryRun" && return
 
     [ ! -d $T_PATH ] && mkdir -p $T_PATH
 
     local BACKUPCONFIG=$(cat $T_PATH/$CONF_NAME 2>/dev/null | egrep -v ^\# | egrep -v ^$)
     if [ "$BACKUPCONFIG" == "" ]; then
-        if [ -f "$script_path/sample-configs/$CONF_NAME" ]
-        then
+        if [ -f "$script_path/sample-configs/$CONF_NAME" ]; then
             cp -f $script_path/sample-configs/$CONF_NAME $T_PATH/$CONF_NAME
             . $T_PATH/$CONF_NAME
         else
             cp -f $script_path/sample-configs/default-backup.conf $T_PATH/$CONF_NAME
         fi
     else
-        LogInfo PHASE 0: Load Variables from $T_PATH/$CONF_NAME
+        LogInfo $op: Load Variables from $T_PATH/$CONF_NAME
         LogInfo ""
         . $T_PATH/$CONF_NAME
     fi
 
     if [ "$create_only" == 1 ]; then
-        if [ -f "$script_path/sample-configs/$CONF_NAME" ]
-        then
+        if [ -f "$script_path/sample-configs/$CONF_NAME" ]; then
             cp -u $script_path/sample-configs/$CONF_NAME $T_PATH/$CONF_NAME
         else
             cp -u $script_path/sample-configs/default-backup.conf $T_PATH/$CONF_NAME
         fi
 
-        LogInfo $T_PATH/$CONF_NAME was created.
+        LogInfo $op: $T_PATH/$CONF_NAME was created.
         return
     fi
 }
 
-
 function archive_docker() {
-
+    local op="[ARCHIVE]"
     [[ "$ARCHIVE_DAYS" == "0" ]] && ARCHIVE_DAYS=1
 
     [ ! -d $A_PATH ] && mkdir -p $A_PATH
 
     [ "$dry_run" == "0" ] && [ -d $A_PATH ] && find $A_PATH -mtime +${ARCHIVE_DAYS} -name '*.tgz' -delete
 
-    LogInfo PHASE 1: Archive $D_PATH to $A_FILE
+    LogInfo $op: Archiving to $A_FILE. This may take some time...
     if [ -d $A_PATH ] && [ ! -f $A_FILE ] && [ -d $D_PATH ]; then
-        LogVerbose PHASE 1: tar -czf $A_FILE -C $D_PATH .
-        if [[ "$dry_run" == "0" ]];then
-           tar -czf $A_FILE -C $D_PATH .
-           if [[ $? -ne 0 ]]; then
-              LogError "tar failed"
-           fi
+        LogVerbose $op: tar -czf $A_FILE -C $D_PATH .
+        if [[ "$dry_run" == "0" ]]; then
+            tar -czf $A_FILE -C $D_PATH .
+            if [[ $? -ne 0 ]]; then
+                LogError "$op: tar failed"
+            fi
         fi
-
     else
-        [ -f $A_FILE ] && LogInfo PHASE 1: Skipped. Archive exists for $now.
-        [ ! -f $A_FILE ] && LogInfo PHASE 1: Skipped. NUM_DAILY [$ARCHIVE_DAYS]
+        [ -f $A_FILE ] && LogInfo $op: Skipped. Archive exists for $now.
+        [ ! -f $A_FILE ] && LogInfo $op: Skipped. NUM_DAILY [$ARCHIVE_DAYS]
     fi
 }
 
 function backup_docker() {
+    local op="[BACKUP DOCKER]"
     local START_TIME=$SECONDS
     local TIMEOUT=$DEFAULT_TIMEOUT
     local D_NAME=$1
@@ -243,7 +243,7 @@ function backup_docker() {
     [ ! -d $D_PATH ] && mkdir -p $D_PATH
 
     local pre_excludes=${exclude_opts_pre[@]}
-    local full_excludes=${exclude_opts[@]}    
+    local full_excludes=${exclude_opts[@]}
 
     if [ ! "$EXCLUDES" == "" ]; then
         for item in "${EXCLUDES[@]}"; do
@@ -252,12 +252,12 @@ function backup_docker() {
         done
     fi
 
-    [ ! "$BACKUP" == "true" ] && LogInfo PHASE 0: Skipping Docker $D_NAME && return
+    [ ! "$BACKUP" == "true" ] && LogInfo $op: Skipping Docker $D_NAME && return
 
     local A_PATH=$T_PATH/Archive
     local A_FILE=$A_PATH/$D_NAME-${now}.tgz
     local RUNNING=$(docker container inspect -f '{{.State.Running}}' $D_NAME)
-    
+
     printf "Dest Path: \t $D_PATH\n"
     printf "Source Path: \t $S_PATH\n"
     printf "Archive Path: \t $A_PATH\n"
@@ -267,85 +267,82 @@ function backup_docker() {
     [ ! "$EXCLUDES" == "" ] && printf "Excludes: \t (${EXCLUDES[*]})\n"
     echo ""
 
-    LogInfo PHASE 2: Run rsync to copy files BEFORE docker stop
-    if [ "$RUNNING" == "true" ] && [ ! "$TIMEOUT" == "0" ]; then        
-        LogVerbose PHASE 2: rsync -a $PROGRESS -h ${pre_excludes[@]} $DRYRUN $S_PATH/ $D_PATH/
+    LogInfo $op: RSYNC Run 1 - Run rsync to copy files BEFORE docker stop
+    if [ "$RUNNING" == "true" ] && [ ! "$TIMEOUT" == "0" ]; then
+        LogVerbose $op: rsync -a $PROGRESS -h ${pre_excludes[@]} $DRYRUN $S_PATH/ $D_PATH/
         rsync -a $PROGRESS -h ${pre_excludes[@]} $DRYRUN $S_PATH/ $D_PATH/
         if [[ $? -ne 0 ]]; then
-           LogWarning "Pre Rsync Failed"
+            LogWarning $op: RSYNC RUN 1 Failed
         fi
         stop_docker $D_NAME $TIMEOUT
     else
-        LogInfo PHASE 2: Skipped Docker Stop because either docker state:$RUNNING is not running or Timeout [$TIMEOUT] is 0.
+        LogInfo $op: Skipped Docker Stop because either docker state [$RUNNING] is not running or Timeout [$TIMEOUT] is 0.
     fi
-    
-    LogInfo PHASE 3: Run rsync to copy files AFTER docker stop
-    LogVerbose PHASE 3: rsync -a $PROGRESS -h ${full_excludes[@]} --delete --delete-excluded $DRYRUN $S_PATH/ $D_PATH/
+
+    LogInfo $op: RSYNC RUN 2 - Run rsync to copy files AFTER docker stop
+    LogVerbose $op: rsync -a $PROGRESS -h ${full_excludes[@]} --delete --delete-excluded $DRYRUN $S_PATH/ $D_PATH/
     rsync -a $PROGRESS -h ${full_excludes[@]} --delete --delete-excluded $DRYRUN $S_PATH/ $D_PATH/
     if [[ $? -ne 0 ]]; then
-       LogError "Full Rsync Failed"
+        LogError "$op: RSYNC RUN 2 Failed"
     fi
-    
-    LogInfo "PHASE 4: Start docker if previously running"
+
+    LogInfo "$op: Start docker if previously running"
 
     local autostart=$(cat /var/lib/docker/unraid-autostart | cut -f 1 -d " " | egrep "^${D_NAME}$")
-    LogInfo autostart = $autostart
+    LogVerbose $op: autostart = $autostart
     if [[ ! "$FORCESTART" == "false" ]] || [[ "$autostart" == "$D_NAME" ]] || [[ "$RUNNING" == "true" && ! "$TIMEOUT" == "0" ]]; then
-       start_docker $D_NAME
+        start_docker $D_NAME
     else
-        LogInfo PHASE 4: Docker Start Skipped. FORCESTART [$FORCESTART], RUNNING [$RUNNING], TIMEOUT [$TIMEOUT]
+        LogInfo $op: Docker Start Skipped. FORCESTART [$FORCESTART], RUNNING [$RUNNING], TIMEOUT [$TIMEOUT]
     fi
 
     archive_docker
 
-echo ""
-    echo End Time: $(date) [Elapsed $(( SECONDS-$START_TIME )) Seconds]
+    echo ""
+    echo End Time: $(date) [Elapsed $((SECONDS - $START_TIME)) Seconds]
     echo =================================================================
     echo ""
+}
+
+# flash drive backup
+function BackupFlash() {
+    local op="[BACKUP FLASH]"
+
+    if [[ ! "$create_only" == "1" && "$docker_name" == "" ]]; then
+        LogInfo $op: Starting Flash Backup...
+        if [[ ! "$dry_run" == "0" ]]; then
+            LogInfo "$op: Skipping /usr/local/emhttp/webGui/scripts/flash_backup in dry run"
+        else
+            backup_file=$(/usr/local/emhttp/webGui/scripts/flash_backup)
+            if [[ $? -ne 0 ]]; then
+                LogError "$op: flash_backup failed"
+            fi
+
+            if [ -f /$backup_file ]; then
+                mkdir -p $BACKUP_LOCATION/Flash/Archive
+                mv /$backup_file $BACKUP_LOCATION/Flash/Archive
+                find $BACKUP_LOCATION/Flash/Archive -mtime +${NUM_DAILY} -name '*.zip' -delete
+            fi
+        fi
+        LogInfo $op: Flash Backup completed.
+    fi
 }
 
 echo ""
 echo "---- Backup Started [$(date)] ----"
 echo ""
 
-# exclude options, phase 2
+# exclude options, RSYNC2
 exclude_opts=()
 for item in "${EXCLUDE[@]}"; do
     exclude_opts+=(--exclude "$item")
 done
 
-# exclude options, phase 1
+# exclude options, RSYNC1
 exclude_opts_pre=${exclude_opts[@]}
 for item in "${EXCLUDEPRE[@]}"; do
     exclude_opts_pre+=(--exclude "$item")
 done
-
-# test
-# backup_docker "organizrv2"
-# exit
-
-# flash drive backup
-function BackupFlash() {
-
-if [[ ! "$create_only" == "1" && "$docker_name" == "" ]]; then
-    LogInfo Starting Flash Backup...
-    if [[ ! "$dry_run" == "0" ]]; then
-       LogInfo "Skipping /usr/local/emhttp/webGui/scripts/flash_backup in dry run"
-    else
-        backup_file=`/usr/local/emhttp/webGui/scripts/flash_backup`
-        if [[ $? -ne 0 ]]; then
-            LogError "flash_backup failed"
-        fi
-
-        if [ -f /$backup_file ]; then
-           mkdir -p $BACKUP_LOCATION/Flash/Archive
-           mv /$backup_file $BACKUP_LOCATION/Flash/Archive
-           find $BACKUP_LOCATION/Flash/Archive -mtime +${NUM_DAILY} -name '*.zip' -delete
-        fi
-    fi
-    LogInfo Flash Backup completed.
-fi
-}
 
 BackupFlash
 
@@ -357,13 +354,13 @@ if [[ "$docker_name" == "" ]]; then
     if [[ "$containers" == "" ]]; then
         containers=$(docker ps -a | awk '{if(NR>1) print $NF}' | sort -f)
     fi
-for container in $containers; do
+    for container in $containers; do
         backup_docker $container
     done
 else
     container=$(docker ps -a | awk '{if(NR>1) print $NF}' | egrep -i ^$docker_name$)
     if [[ ! "$container" == "" ]]; then
-        backup_docker $docker_name
+        backup_docker $container
     else
         LogWarning Could not find $docker_name. Run docker ps -a command to check.
         echo
@@ -380,19 +377,19 @@ fi
 
 echo "---- Starting Onedrive upload [$(date)] ----"
 echo ""
+op="[RCLONE]"
 RCLONE="/usr/sbin/rclone sync -v --exclude Live/** --onedrive-chunk-size 70M --retries 1 --checkers 16 --transfers 6 --fast-list --copy-links"
 if [[ $script_path =~ \/boot\/repos.* ]]; then # one-line stats when running from user scripts
-    LogInfo $RCLONE $BACKUP_LOCATION $ONEDRIVE_LOCATION
-    LogInfo rclone is working. Waiting...
+    LogInfo $op: $RCLONE $BACKUP_LOCATION $ONEDRIVE_LOCATION
+    LogInfo $op: rclone is working. Waiting...
     $RCLONE $BACKUP_LOCATION $ONEDRIVE_LOCATION
 else
-    LogVerbose $RCLONE --progress $BACKUP_LOCATION $ONEDRIVE_LOCATION
+    LogVerbose $op: $RCLONE --progress $BACKUP_LOCATION $ONEDRIVE_LOCATION
     $RCLONE --progress $BACKUP_LOCATION $ONEDRIVE_LOCATION
 fi
 if [[ $? -ne 0 ]]; then
-    LogError "rclone failed"
+    LogError "$op: rclone failed"
 fi
-
 
 SUCCESS="true"
 echo ""
