2ca2544b05b7ae93bdfff542283871dda3a9b640
doctarrwho <doctarrwho@outlook.com>
2020-11-16 15:28:07 -0500

    git pull changes

--- user.script.sh
+++ user.script.sh
@@ -8,7 +8,7 @@ SCRIPT_NAME=backuparr.sh
 trap "kill -- $$" exit SIGINT SIGTERM SIGHUP SIGPIPE SIGQUIT
 
 [ ! -d $REPO_LOCATION ] && mkdir -p $REPO_LOCATION && cd $REPO_LOCATION && git clone $REPO_URL
-cd $REPO_LOCATION/$REPO_NAME && git pull
+cd $REPO_LOCATION/$REPO_NAME && git fetch --all && git reset --hard origin/master && git pull --ff-only
 chmod +x $REPO_LOCATION/$REPO_NAME/$SCRIPT_NAME
 exec /bin/bash $REPO_LOCATION/$REPO_NAME/$SCRIPT_NAME -u -o onedrive:unraid/backup
 # Example for specifying backup path: exec /bin/bash $REPO_LOCATION/$REPO_NAME/$SCRIPT_NAME -u -o onedrive:unraid/backup -b /path/to/backup
